bf5009d0b2a4df21ac6c3739036219dd1cccc33e
Benjamin Paz <benjapaz57@gmail.com>
2021-09-15 00:24:49 -0300

    Creando Repo

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let nombre = prompt('Ingrese su nombre')
+let apellido = prompt('Ingrese su apellido')
+let edad = prompt('Ingrese su edad')
+
+let resultado = alert('Bienvenide' + ' ' + nombre + ' ' + apellido + ',' + ' ' + edad + ' ' + 'años' + '.')
+console.log('Bienvenide' + ' ' + nombre + ' ' + apellido)
