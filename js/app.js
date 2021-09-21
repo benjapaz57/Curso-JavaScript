e15f5dea1a2e36c6e1ac0d7f2778dcc425aa201a
Benjamin Paz <benjapaz57@gmail.com>
2021-09-20 23:58:03 -0300

    Clase 3

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
-let edad = prompt('Ingrese su edad')
-let edadPareja = prompt('Ingrese la edad de su pareja')
-if (edad > edadPareja){
-    console.log('Usted es mayor que su pareja.')
-} else if (edad < edadPareja){
-    console.log('Su pareja es mayor que usted')
-} else{
-    console.log('Tienen la misma edad')
-}
+//El ejemplo serían las mesas de un bar
+let numMesa = Number(prompt('Ingrese cantidad de mesas ocupadas'))
+let i = 1
+do{
+    switch (numMesa){
+        case 2:
+            console.log('Poca gente')
+            break
+        case 5: 
+            console.log('Mitad del bar lleno')
+            break
+        case 8: 
+            console.log('Casi casi lleno')
+            break
+        case 10:
+            console.log('Bar lleno')
+    }
+    i++
+} while (i<=10)
+let resultado = alert('Hay ' + numMesa + ' mesas ocupadas')
