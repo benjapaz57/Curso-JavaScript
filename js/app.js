373ee275e98d454dd4cbe32dac607608f964440e
Benjamin Paz <benjapaz57@gmail.com>
2021-10-04 20:27:44 -0300

    Clase 5

--- js/app.js
+++ js/app.js
@@ -1,22 +1,43 @@
-//Ejemplo act complementaria funciones
-function solicitarMonto (){
-    let monto = Number(prompt('Ingrese el monto.'))
-    let porcentaje = Number(prompt('Ingrese el % a ganar'))
-    
-    operacion (monto, porcentaje);
-
+class Pacientes {
+constructor(nombre, edad, inicio, mail, frecuencia) {
+    (this.nombre = nombre),
+    (this.edad = edad),
+    (this.inicio = inicio),
+    (this.mail = mail),
+    (this.frecuencia = frecuencia);
 }
-
-function operacion (a, b){
-    let multiplicacion = a * b
-    let division = multiplicacion / 100
-    let suma = division + a
-    resultado (suma)
+turnos(turno) {
+    if (turno <= this.frecuencia) {
+    console.log("No puedes guardar un turno");
+    alert("No puedes guardar un turno");
+    } else {
+    console.log("Guardaste un turno correctamente");
+    alert("Guardaste un turno correctamente");
+    }
 }
-
-function resultado (a){
-    console.log(a)
-    alert (a + " Es el monto final")
 }
-
-solicitarMonto();
+const pacienteUno = new Pacientes(
+"Benjamin Paz",
+"26 años",
+"Mayo 2020",
+"pacienteUno@mail.com",
+"15 dias"
+);
+const pacienteDos = new Pacientes(
+"Juan Perez",
+"30 años",
+"Enero 2019",
+"pacienteDos@mail.com",
+"30 dias"
+);
+const pacienteTres = new Pacientes(
+"María Diaz",
+"19 años",
+"Marzo 2021",
+"pacienteTres@mail.com",
+"7 dias"
+);
+let turnero = prompt("¿Dentro de cuantos dias es su turno?");
+console.log(pacienteUno);
+console.log(pacienteDos.frecuencia);
+pacienteDos.turnos(turnero);
