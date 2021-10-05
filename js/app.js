6d9a179baafd9096a67d1078757491d70a80dbae
Benjamin Paz <benjapaz57@gmail.com>
2021-10-05 20:59:30 -0300

    Clase 6 Arrays

--- js/app.js
+++ js/app.js
@@ -1,43 +1,31 @@
+//Clase 6 arrays
+
 class Pacientes {
-constructor(nombre, edad, inicio, mail, frecuencia) {
-    (this.nombre = nombre),
-    (this.edad = edad),
-    (this.inicio = inicio),
-    (this.mail = mail),
-    (this.frecuencia = frecuencia);
-}
-turnos(turno) {
-    if (turno <= this.frecuencia) {
-    console.log("No puedes guardar un turno");
-    alert("No puedes guardar un turno");
-    } else {
-    console.log("Guardaste un turno correctamente");
-    alert("Guardaste un turno correctamente");
+    constructor(nombre, mail){
+        this.nombre = nombre;
+        this.mail = mail;
     }
+
 }
+
+let datosPacientes = [];
+
+const agregarPacientes = () => {
+    let nombre = prompt('Ingrese el nombre y apellido del paciente')
+    let mail = prompt('Ingrese el mail del paciente')
+
+    let paciente = new Pacientes (nombre, mail)
+    datosPacientes.push(paciente)
 }
-const pacienteUno = new Pacientes(
-"Benjamin Paz",
-"26 años",
-"Mayo 2020",
-"pacienteUno@mail.com",
-"15 dias"
-);
-const pacienteDos = new Pacientes(
-"Juan Perez",
-"30 años",
-"Enero 2019",
-"pacienteDos@mail.com",
-"30 dias"
-);
-const pacienteTres = new Pacientes(
-"María Diaz",
-"19 años",
-"Marzo 2021",
-"pacienteTres@mail.com",
-"7 dias"
-);
-let turnero = prompt("¿Dentro de cuantos dias es su turno?");
-console.log(pacienteUno);
-console.log(pacienteDos.frecuencia);
-pacienteDos.turnos(turnero);
+
+datosPacientes.sort((a, b) => {
+    if (a.nombre > b.nombre){
+        return 1
+    }
+    if (a.nombre < b.nombre){
+        return -1
+    }
+    return 0
+})
+
+console.log(datosPacientes)
